160b8f911dc95248bb1717375a226b5d66a3e5a8
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-04-21 19:40:34 +0200

    13th_commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,17 @@ import { GardenModule } from './garden/garden.module';
 import { AuthModule } from './auth/auth.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname , ".." , "./uploads")
-    }),
     ConfigModule.forRoot({isGlobal: true}),
     SequelizeModule.forRoot({
       dialect: process.env.DIALECT_USERNAME as any,
       host: process.env.HOST,
       port: process.env.PORT as any,
       username: process.env.DIALECT_USERNAME as any,
-      password: process.env.PASSWORD as any,
+      password: process.env.PASSWORD as any, 
       database: process.env.DATABASE as any,
       autoLoadModels: true,
       synchronize: true,
